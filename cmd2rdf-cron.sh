a000b24f57d9ad7742a6a28ca430f6e1a0207347
Menzo <menzo.windhouwer@meertens.knaw.nl>
2017-09-07 16:53:18 +0200

    When there is no new set available we're left with a .BAK, pick that up next time.

--- cmd2rdf-cron.sh
+++ cmd2rdf-cron.sh
@@ -7,14 +7,18 @@ DATA="/app/data/clarin"
 SRC="https://vlo.clarin.eu/data/resultsets"
 SET="clarin.tar.bz2"
 
-mkdir -p $WORK
 # make sure expected dirs exist
+mkdir -p $WORK
 mkdir -p $WORK/harvester
 mkdir -p $WORK/profiles-cache
 mkdir -p $WORK/rdf-output/temp
+
+# check if there is a new resultset
 cd $WORK
 if [ -f $SET ]; then
     mv $SET $SET.BAK
+fi
+if [ -f $SET.BAK ]; then
     curl -o $SET -z $SET.BAK "$SRC/$SET"
 else
     curl -o $SET "$SRC/$SET"
@@ -25,7 +29,7 @@ if [ ! -f $SET ]; then
     exit
 fi
 
-# refresh
+# refresh data
 if [ -d $DATA ]; then
     rm -rf $DATA
 fi
